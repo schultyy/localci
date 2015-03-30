10df41128ab3263fd361a77e9f775f93c5fbb745
Jan Schulte <hello@unexpected-co.de>
2015-03-30 16:16:43 +0200

    Move app code into app module

--- lib/localci.ex
+++ lib/localci.ex
@@ -3,44 +3,7 @@ require JSON
 
 defmodule Localci do
   def main(args) do
-    params = parse_args(args)
-    config = params[:configfile]
-      |> load_config
-      |> remove
-      |> clone
-      |> build
-
-    Logger.info inspect(config)
-  end
-
-  def remove(config) do
-    %{ "name" => name } = config
-    Localci.Repository.remove(name)
-    config
-  end
-
-  defp clone(config) do
-    %{ "repository" => url } = config
-    %{ "name" => name } = config
-    Localci.Repository.clone(url, name)
-    config
-  end
-
-  def build(config) do
-    %{ "name" => name } = config
-    %{ "command" => command} = config
-
-    command_list = cond do
-      is_list(command) -> command
-      true -> [command]
-    end
-
-    Enum.each(command_list, fn(cmd) -> Localci.Build.execute(String.split(cmd), name) end)
-  end
-
-  defp load_config(filename) do
-    {:ok, content} = File.read(filename)
-    LocalCi.Configuration.parse(content)
+    parse_args(args) |> Localci.App.run
   end
 
   defp parse_args(args) do
---
+++ lib/localci_app.ex
@@ -0,0 +1,39 @@
+defmodule Localci.App do
+  def run(params) do
+    config = params[:configfile]
+      |> load_config
+      |> remove
+      |> clone
+      |> build
+  end
+
+  defp remove(config) do
+    %{ "name" => name } = config
+    Localci.Repository.remove(name)
+    config
+  end
+
+  defp clone(config) do
+    %{ "repository" => url } = config
+    %{ "name" => name } = config
+    Localci.Repository.clone(url, name)
+    config
+  end
+
+  defp build(config) do
+    %{ "name" => name } = config
+    %{ "command" => command} = config
+
+    command_list = cond do
+      is_list(command) -> command
+      true -> [command]
+    end
+
+    Enum.each(command_list, fn(cmd) -> Localci.Build.execute(String.split(cmd), name) end)
+  end
+
+  defp load_config(filename) do
+    {:ok, content} = File.read(filename)
+    LocalCi.Configuration.parse(content)
+  end
+end
